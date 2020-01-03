0f6d2d1a4c8d95f207bd721a871423c9fde18d39
Olexandr Trykopa <55888517+trykopa@users.noreply.github.com>
2020-01-03 15:03:02 +0200

    xxx

--- src/main/java/ua/kiev/prog/DBSettings.java
+++ src/main/java/ua/kiev/prog/DBSettings.java
@@ -8,7 +8,7 @@ public class DBSettings {
     public static final String driver = "com.mysql.cj.jdbc.Driver";
     public static final String host = "jdbc:mysql://127.0.0.1:3306/test";
     public static final String user = "root";
-    public static final String password = "mazafaka";
+    public static final String password = "xxx";
 
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName(driver);
